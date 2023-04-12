3ea7b1a167fc22cf52d9c1a738174be94db8391d
Alex Lucchesi <lucchesia7@gmail.com>
2023-04-12 13:50:41 -0700

    update

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+from base import Base
+from dotenv import load_dotenv
+import pymongo
+import os
+
+load_dotenv()
+mongo_url = os.getenv('MONGO_URL')
+client = pymongo.MongoClient(mongo_url)
+db = client.db
+cards = db.cards
+base = Base()
+df = base.df
+df.set_index('id', inplace=True)
+for i in df.index:
+    cards.insert_one(df.loc[i].to_dict())
