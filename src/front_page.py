bf3a552045bd9f465cc53dd334614a718b57159d
Alex Lucchesi <lucchesia7@gmail.com>
2023-04-13 13:25:16 -0700

    update

--- src/front_page.py
+++ src/front_page.py
@@ -1,16 +1,16 @@
 import streamlit as st # Alias our streamlit import as st
 
 st.title('MTG Tracer')
-st.text('My first application which uses\n')
+st.text('My first application which uses')
 st.text('pandas, streamlit, mongo, and Python to marry together the application')
 
-st.header('Here are the different pages of my application!:')
+st.header('Here are the different pages of my application!')
 
+st.subheader('Image Return')
 st.text('Image Return: Returns an image of the requested card')
-st.text("Recommender: Uses a NN model to return recommended cards")
+
+st.subheader('Visualizer')
+st.text("Summary: Shares summary of research")
+
+st.subheader('recommender')
 st.text("Visualizer: Allows users to view information about cards")
-
-
-st.title('Image Return')
-st.title('Visualizer')
-st.title('recommender')
--- src/pages/img_return.py
+++ src/pages/img_return.py
@@ -2,20 +2,17 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 import requests
-
-st.title('Image Return')
-
-
 from pathlib import Path
-
 import os
 import sys
 import pandas as pd
+
 filepath = os.path.join(Path(__file__).parents[1])
 sys.path.insert(0, filepath)
-
 from to_mongo import ToMongo
+
+st.title('Image Return')
 c = ToMongo()
-answer = st.text_input('Enter Card Name')
+answer = st.text_input('Enter Card Name', value='Sol Ring')
 card = list(c.cards.find({'name':answer}))[0]['image_uris']['normal']
 st.image(Image.open(BytesIO(requests.get(card).content)))
